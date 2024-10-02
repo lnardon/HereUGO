34819f2797d76ea60a996d09514e06e4c6dde66f
lnardon <lucaslnardon@gmail.com>
2024-10-02 16:14:34 -0300

    Remove frontend for now

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM node:22.9.0-alpine3.19 AS build-frontend
-WORKDIR /app
-COPY ./frontend .
-RUN npm install
-RUN npm run build
+# FROM node:22.9.0-alpine3.19 AS build-frontend
+# WORKDIR /app
+# COPY ./frontend .
+# RUN npm install
+# RUN npm run build
 
 FROM golang:1.23.1-alpine3.19 AS build-backend
 WORKDIR /app
 COPY . .
-COPY --from=build-frontend /app/dist /app/frontend/dist
+# COPY --from=build-frontend /app/dist /app/frontend/dist
 RUN go build -o main *.go
 
 FROM alpine:3.19 AS server
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main(){
-    http.Handle("/", http.FileServer(http.Dir("./frontend/dist")))
+    // http.Handle("/", http.FileServer(http.Dir("./frontend/dist")))
 
     http.HandleFunc("/api/uploadFile", handleUploadFile)
     http.HandleFunc("/api/getSharedFile", handleGetSharedFile)
 
     const PORT = ":8080"
     err := http.ListenAndServe(PORT, nil)
+    fmt.Println("Server started at port", PORT)
     if err != nil {
         log.Println("Error starting server")
         return
